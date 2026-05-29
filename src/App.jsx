83741fc02e2e4003fd73f1121eff58a988a4bc57
Musa <Musaxlilov29@gmail.com>
2026-05-29 11:26:30 +0400

    Buttons logic add

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,33 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [isActiveDensity, setIsActiveDensity] = useState(0)
+  const [isActiveOctane, setIsActiveOctane] = useState(0)
+  const [isActiveErr, setIsActiveErr] = useState(0)
 
   return (
     <>
       <section id="center">
         <ul className='buttons'>
-          <li>Sıxlıq hesablayıcısı</li>
-          <li>Oktan ədədinin müqayisəsi</li>
-          <li>Ölçmə xətasının hesablanması</li>
+          <li onClick={() => {
+            setIsActiveDensity(1);
+            setIsActiveErr(0);
+            setIsActiveOctane(0);
+          }}>Sıxlıq hesablayıcısı</li>
+          <li onClick={() => {
+            setIsActiveDensity(0);
+            setIsActiveErr(0);
+            setIsActiveOctane(1);
+          }}>Oktan ədədinin müqayisəsi</li>
+          <li onClick={() => {
+            setIsActiveDensity(0);
+            setIsActiveErr(1);
+            setIsActiveOctane(0);
+          }}>Ölçmə xətasının hesablanması</li>
         </ul>
+        <div>
+          {isActiveDensity ? <div>density calculator</div> : isActiveErr? <div>Error calculator</div> : isActiveOctane? <div>Octane calculator</div> : <></>}
+        </div>
       </section>
     </>
   )
